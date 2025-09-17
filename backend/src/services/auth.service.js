d50e5ac60d48c4bc27a119c186e12f203a9196ce
Sahil Yatam <sahilyatam9@gmail.com>
2025-09-17 19:08:10 +0530

    feat(service): add forget-reset password functionality and other few services

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,6 +1,7 @@
 import { User } from "../models/User.js"
 import { ApiError } from '../utils/ApiError.js';
-import {hashPassword, comparePassword} from "../utils/password.utils.js"
+import { Session } from "../models/Session.model.js";
+import {hashPassword, comparePassword, hashToken} from "../utils/password.utils.js"
 
 const register = async(data) => {
     const email = data.email.toLowerCase().trim();
@@ -41,7 +42,83 @@ const login = async (data) => {
     return {userId: user._id, email: user.email, name: user.name}
 }
 
+const logout = async (refreshToken) => {
+    const hashedToken = hashToken(refreshToken)
+    const session = await Session.findOneAndDelete({refreshToken: hashedToken});
+
+    if(!session) throw new ApiError(403, "Unauthorized request");
+
+    return { message: "Session invalidated successfully" };
+}
+
+const getUser = async (id) => {
+    const user = await User.findById(id).select("-password")
+    if(!user) throw new ApiError(404, "User not found");
+
+    return user;
+}
+
+const changePassword = async(id, data) => {
+    const user = await User.findById(id)
+    if(!user) throw new ApiError(404, "User not found");
+
+    const isPasswordValid = await comparePassword(data.oldPassword, user.password)
+    if(!isPasswordValid) throw new ApiError(401, "Password is incorrect");
+
+    if(data.newPassword !== data.confirmPassword) {
+        throw new ApiError(401, "Password does not match with confirm password");
+    }
+
+    const newPassword = await hashPassword(data.newPassword)
+
+    user.password = newPassword;
+
+    await user.save();
+
+    return {message: "Password changed successfully"};
+}
+
+const forgetPasswordRequest = async (data) => {
+    const email = data.email.toLowerCase().trim();
+    const user = await User.findOne({email});
+
+    if(!user) throw new ApiError(404, "User with this email does not exist");
+
+    const rawToken = generateToken();
+    const resetTokenExpiresAt = new Date(Date.now() + 10 * 60 * 1000);
+
+    const hashedToken = hashToken(rawToken)
+
+    user.resetPasswordToken = hashedToken;
+    user.resetPasswordExpiresAt = resetTokenExpiresAt;
+
+    await user.save();
+
+    return {email: user.email, token: rawToken, userId: user._id}
+}
+
+const resetPassword = async(token, password) => {
+    const hashedToken = hashToken(token)
+    const user = await User.findOne({resetPasswordToken: hashedToken})
+
+    if(!user || user.resetPasswordExpiresAt.getTime()  < Date.now()) {
+        throw new ApiError(403, "Invalid or Expired reset token");
+    }
+
+    const newPassword = await hashPassword(password)
+    user.password = newPassword;
+
+    await user.save();
+
+    return {email: user.email, userId: user._id};
+}
+
 export const authService = {
     register,
-    login
+    login,
+    logout,
+    getUser,
+    changePassword,
+    forgetPasswordRequest,
+    resetPassword
 }
